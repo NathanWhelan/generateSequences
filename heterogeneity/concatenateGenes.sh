dbd79248a8c469167a9485c9bdb465039efcb796
Nathan Whelan <NathanWhelan@users.noreply.github.com>
2016-03-24 17:51:11 -0500

    Create concatenateGenes.sh

---
+++ heterogeneity/concatenateGenes.sh
@@ -0,0 +1,340 @@
+#!/bin/bash
+###This script was specifically made for 250 and three randomly being combined until none were left to combine. Clearly, there are better ways to do this...
+
+mkdir concatenatedThreeGenes
+cd 1
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 1.fas
+mv 1.fas ../concatenatedThreeGenes/
+
+cd ../2
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 2.fas
+mv 2.fas ../concatenatedThreeGenes/
+
+cd ../3
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 3.fas
+mv 3.fas ../concatenatedThreeGenes/
+
+cd ../4
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 4.fas
+mv 4.fas ../concatenatedThreeGenes/
+
+cd ../5
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 5.fas
+mv 5.fas ../concatenatedThreeGenes/
+
+cd ../6
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 6.fas
+mv 6.fas ../concatenatedThreeGenes/
+
+cd ../7
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 7.fas
+mv 7.fas ../concatenatedThreeGenes/
+
+cd ../8
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 8.fas
+mv 8.fas ../concatenatedThreeGenes/
+
+cd ../9
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 9.fas
+mv 9.fas ../concatenatedThreeGenes/
+
+cd ../10
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 10.fas
+mv 10.fas ../concatenatedThreeGenes/
+
+cd ../11
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 11.fas
+mv 11.fas ../concatenatedThreeGenes/
+
+cd ../12
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 12.fas
+mv 12.fas ../concatenatedThreeGenes/
+
+cd ../13
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 13.fas
+mv 13.fas ../concatenatedThreeGenes/
+
+cd ../14
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 14.fas
+mv 14.fas ../concatenatedThreeGenes/
+
+cd ../15
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 15.fas
+mv 15.fas ../concatenatedThreeGenes/
+
+cd ../16
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 16.fas
+mv 16.fas ../concatenatedThreeGenes/
+
+cd ../17
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 17.fas
+mv 17.fas ../concatenatedThreeGenes/
+
+cd ../18
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 18.fas
+mv 18.fas ../concatenatedThreeGenes/
+
+cd ../19
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 19.fas
+mv 19.fas ../concatenatedThreeGenes/
+
+cd ../20
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 20.fas
+mv 20.fas ../concatenatedThreeGenes/
+
+cd ../21
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 21.fas
+mv 21.fas ../concatenatedThreeGenes/
+
+cd ../22
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 22.fas
+mv 22.fas ../concatenatedThreeGenes/
+
+cd ../23
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 23.fas
+mv 23.fas ../concatenatedThreeGenes/
+
+cd ../24
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 24.fas
+mv 24.fas ../concatenatedThreeGenes/
+
+cd ../25
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 25.fas
+mv 25.fas ../concatenatedThreeGenes/
+
+cd ../26
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 26.fas
+mv 26.fas ../concatenatedThreeGenes/
+
+cd ../27
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 27.fas
+mv 27.fas ../concatenatedThreeGenes/
+
+cd ../28
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 28.fas
+mv 28.fas ../concatenatedThreeGenes/
+
+cd ../29
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 29.fas
+mv 29.fas ../concatenatedThreeGenes/
+
+cd ../30
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 30.fas
+mv 30.fas ../concatenatedThreeGenes/
+
+cd ../31
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 31.fas
+mv 31.fas ../concatenatedThreeGenes/
+
+cd ../32
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 32.fas
+mv 32.fas ../concatenatedThreeGenes/
+
+cd ../33
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 33.fas
+mv 33.fas ../concatenatedThreeGenes/
+
+cd ../34
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 34.fas
+mv 34.fas ../concatenatedThreeGenes/
+
+cd ../35
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 35.fas
+mv 35.fas ../concatenatedThreeGenes/
+
+cd ../36
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 36.fas
+mv 36.fas ../concatenatedThreeGenes/
+
+cd ../37
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 37.fas
+mv 37.fas ../concatenatedThreeGenes/
+
+cd ../38
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 38.fas
+mv 38.fas ../concatenatedThreeGenes/
+
+cd ../39
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 39.fas
+mv 39.fas ../concatenatedThreeGenes/
+
+cd ../40
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 40.fas
+mv 40.fas ../concatenatedThreeGenes/
+
+cd ../41
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 41.fas
+mv 41.fas ../concatenatedThreeGenes/
+
+cd ../42
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 42.fas
+mv 42.fas ../concatenatedThreeGenes/
+
+cd ../43
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 43.fas
+mv 43.fas ../concatenatedThreeGenes/
+
+cd ../44
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 44.fas
+mv 44.fas ../concatenatedThreeGenes/
+
+cd ../45
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 45.fas
+mv 45.fas ../concatenatedThreeGenes/
+
+cd ../46
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 46.fas
+mv 46.fas ../concatenatedThreeGenes/
+
+cd ../47
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 47.fas
+mv 47.fas ../concatenatedThreeGenes/
+
+cd ../48
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 48.fas
+mv 48.fas ../concatenatedThreeGenes/
+
+cd ../49
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 49.fas
+mv 49.fas ../concatenatedThreeGenes/
+
+cd ../50
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 50.fas
+mv 50.fas ../concatenatedThreeGenes/
+
+cd ../51
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 51.fas
+mv 51.fas ../concatenatedThreeGenes/
+
+cd ../52
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 52.fas
+mv 52.fas ../concatenatedThreeGenes/
+
+cd ../53
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 53.fas
+mv 53.fas ../concatenatedThreeGenes/
+
+cd ../54
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 54.fas
+mv 54.fas ../concatenatedThreeGenes/
+
+cd ../55
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 55.fas
+mv 55.fas ../concatenatedThreeGenes/
+
+cd ../56
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 56.fas
+mv 56.fas ../concatenatedThreeGenes/
+
+cd ../57
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 57.fas
+mv 57.fas ../concatenatedThreeGenes/
+
+cd ../58
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 58.fas
+mv 58.fas ../concatenatedThreeGenes/
+
+cd ../59
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 59.fas
+mv 59.fas ../concatenatedThreeGenes/
+
+cd ../60
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 60.fas
+mv 60.fas ../concatenatedThreeGenes/
+
+cd ../61
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 61.fas
+mv 61.fas ../concatenatedThreeGenes/
+
+cd ../62
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 62.fas
+mv 62.fas ../concatenatedThreeGenes/
+
+cd ../63
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 63.fas
+mv 63.fas ../concatenatedThreeGenes/
+
+cd ../64
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 64.fas
+mv 64.fas ../concatenatedThreeGenes/
+
+cd ../65
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 65.fas
+mv 65.fas ../concatenatedThreeGenes/
+
+cd ../66
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 66.fas
+mv 66.fas ../concatenatedThreeGenes/
+
+cd ../67
+FASconCAT_v1.0.pl -p -s
+mv FcC_smatrix.fas 67.fas
+mv 67.fas ../concatenatedThreeGenes/
+
+mv ../concatenatedThreeGenes/ ../concatenatedThreeGenes/
